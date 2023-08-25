73e20a7de0f3f641260443717bc99459fd8240c5
deBinaryStar <141916126+deBinaryStar@users.noreply.github.com>
2023-08-25 15:54:40 +0300

    Add files via upload

---
+++ Bc_calender.py
@@ -0,0 +1,11 @@
+
+from calendar import*
+year = int(input("Enter Year"))
+print(calendar(year,2,1,8,3))
+
+#2 = 2 Characters for days (Mo, Tu, We,Th,Fr,Sa,Su)
+#1 = 1 row (Line) for each week
+#8 = 8 Rows  for each month
+#3 = 3 Columns   for   all months of the  year 
+#WilsonTheBinaryStar
+
